149ac5b543b61c2424d4528c4c29536d52a58568
kevin0391 <51764146+kevin0391@users.noreply.github.com>
2019-06-12 15:18:57 -0600

    Cargar Negocio

---
+++ Negocio/EmpleadoNegocio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using Entidad;
+
+namespace Negocio
+{
+    public class EmpleadoNegocio
+    {
+        public bool CrearEmpleado(EmpleadoEntidad empleadoEntidad) => new EmpleadoDatos().CrearEmpleado(empleadoEntidad);
+    }
+}
---
+++ Negocio/UsuarioNegocio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+using Entidad;
+
+namespace Negocio
+{
+    public class UsuarioNegocio
+    {
+        public bool Autenticacion(UsuarioEntidad usuarioEntidad) => new UsuarioDatos().Autenticacion(usuarioEntidad);
+
+        public bool Validacion(UsuarioEntidad usuarioEntidad) => new UsuarioDatos().Validacion(usuarioEntidad);
+
+        public string ValidarToken(string token) => new UsuarioDatos().ValidarToken(token);
+
+        public bool RestablecerContraseña(string contraseña, string token) => new UsuarioDatos().RestablecerContraseña(contraseña, token);
+    }
+}
